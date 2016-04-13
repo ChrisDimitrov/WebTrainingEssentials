ae0bea9990e72d90a399f57cec51be9490dcbf72
Kristiyan Dimitrov <kristiyan.dimitrov@corp.accedia.com>
2016-04-13 09:41:11 +0300

    Inital commit.

---
+++ TestMVC/Scripts/Calculator.js
@@ -0,0 +1,49 @@
+﻿
+var Calculator = (function () {
+
+   /**
+    * Wire the events corresponding to the number button.
+    */
+    function wireButtonEvents() {
+        wireNumberButtonEvents();
+        wireFunctionButtonEvents();
+    }
+
+    function wireNumberButtonEvents() {
+        $("#number0").off("click").on("click", buttonClickHandler);
+        $("#number1").off("click").on("click", buttonClickHandler);
+        $("#number2").off("click").on("click", buttonClickHandler);
+        $("#number3").off("click").on("click", buttonClickHandler);
+        $("#number4").off("click").on("click", buttonClickHandler);
+        $("#number5").off("click").on("click", buttonClickHandler);
+        $("#number6").off("click").on("click", buttonClickHandler);
+        $("#number7").off("click").on("click", buttonClickHandler);
+        $("#number8").off("click").on("click", buttonClickHandler);
+        $("#number9").off("click").on("click", buttonClickHandler);
+    }
+
+    function wireFunctionButtonEvents() {
+
+    }
+
+    function buttonClickHandler(e) {
+        var value = e.value;
+        var number = parseInput(value);
+
+        document.getElementById("input").innerText += number;
+    }
+
+    function functionClickHandler(e) {
+
+    }
+
+    function parseInput(input) {
+        return Number.parseFloat(input);
+    }
+
+    return {
+
+    }
+
+})();
+
---
+++ TestMVC/Scripts/Calculator/Calculator.js
@@ -0,0 +1,8 @@
+﻿function parseValue(input) {
+    return Number.parseFloat(input)
+}
+
+function calculate(operand1, operand2, operator)
+{
+
+}
