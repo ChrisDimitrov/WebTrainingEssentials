c122cf48f1c063dd54edc6887feeda933558525d
ChrisDimitrov <krisko92@gmail.com>
2016-04-14 16:59:24 +0300

    Initial commit.

--- WebServices/Properties/AssemblyInfo.cs
+++ WebServices/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebEssentials")]
+[assembly: AssemblyTitle("WebServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebEssentials")]
+[assembly: AssemblyProduct("WebServices")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5d7a95bc-0237-4ced-8dec-e85e6d721f27")]
+[assembly: Guid("5d83b071-1f1f-4e61-845b-4d24740db078")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebTechnologies/Controllers/Training/Html5Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTechnologies.Controllers.Training
+{
+    public class Html5Controller : Controller
+    {
+        // GET: Html5
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Forms()
+        {
+            return View();
+        }
+    }
+}
